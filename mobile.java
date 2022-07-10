393ec909ef873341ede4aaeaa385fa825e4feaba
Shanmugam27 <109014361+Shanmugam27@users.noreply.github.com>
2022-07-10 11:22:03 +0530

    Add files via upload

---
+++ mobile.java
@@ -0,0 +1,20 @@
+package Week1Day1;
+
+public class mobile {
+	// method
+	public void makeCall() {
+		//print
+	}
+	
+	//method
+	public void sendMsg() {
+		//print
+    
+	}
+	
+     // main method
+	public static void main(String[] args) {
+		System.out.println("This is my mobile");
+	
+	}
+}
